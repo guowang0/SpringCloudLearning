2681f8c2987db4d93c6578e52969b1c9b2bb6f43
guowang <gw@jiushig.com>
2017-10-26 15:50:18 +0800

    socke 学习

--- Cloud-Client/src/main/java/com/example/CloudClient/controller/TestController.java
+++ Cloud-Client/src/main/java/com/example/CloudClient/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.example.CloudClient.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
 
 /**
  * Created by zk on 2017/10/9.
@@ -11,10 +16,33 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TestController {
 
+    @Resource
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @RequestMapping("/test")
     public ModelAndView test(){
         ModelAndView modelAndView = new ModelAndView("/index");
         modelAndView.addObject("test","3243234234");
         return modelAndView;
+    }
+
+    @RequestMapping("/receive")
+    public ModelAndView receive(){
+        ModelAndView modelAndView = new ModelAndView("/receive");
+        return modelAndView;
+    }
+
+
+
+//    @MessageMapping("/change-notice")
+//    public void greeting(String value){
+//        this.simpMessagingTemplate.convertAndSend("/topic/notice", value);
+//    }
+
+
+    @MessageMapping("/change-notice")
+    @SendTo("/topic/notice")
+    public String greeting(String value) {
+        return value;
     }
 }
